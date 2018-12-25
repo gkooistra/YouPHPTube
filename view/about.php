b9259ac2a1cb6fcd66aaf942e2e0c74e1150acd0
gkooistra <gradus@kooistrag.nl>
2018-12-25 11:54:26 +0100

    Update view/about.php
    Changed About page

--- view/about.php
+++ view/about.php
@@ -29,25 +29,15 @@ if(!isset($global['systemRootPath'])){
                 }
                 if(empty($custom)){
                 ?>
-                <h1><?php echo __("I would humbly like to thank God for giving me the necessary knowledge, motivation, resources and idea to be able to execute this project. Without God's permission this would never be possible."); ?></h1>
+                <h1><?php echo __("Video's gemaakt door INCOTECH worden hier gehost. | Videos created by INCOTECH are hosted here."); ?></h1>
                 <blockquote class="blockquote">
-                    <h1><?php echo __("For of Him, and through Him, and to Him, are all things: to whom be glory for ever. Amen."); ?></h1>
+                    <h1><?php echo __("Door aanpassingen in het YouTube beleid zijn we begonnen de video's ook zelf te hosten. <br> <hr> <br> Due to changes to the YouTube policy, we started hosting the videos ourselves. "); ?></h1>
                     <footer class="blockquote-footer">Apostle Paul in <cite title="Source Title">Romans 11:36</cite></footer>
                 </blockquote>
                 <div class="btn-group btn-group-justified">
-                    <a href="https://www.youphptube.com/" class="btn btn-success">Main Site</a>
-                    <a href="https://demo.youphptube.com/" class="btn btn-danger">Demo Site</a>
-                    <a href="https://tutorials.youphptube.com/" class="btn btn-primary">Tutorials Site</a>
-                    <a href="https://github.com/DanielnetoDotCom/YouPHPTube/issues" class="btn btn-warning">Issues and requests Site</a>
+                    <a href="https://www.incotech.online/" class="btn btn-success">Main Site</a>
+                    <a href="https://www.youtube.com/c/incotechonline" class="btn btn-danger">YouTube</a>
                 </div>
-                <span class="label label-success"><?php printf(__("You are running YouPHPTube version %s!"), $config->getVersion()); ?></span>
-
-                <span class="label label-success">
-                    <?php printf(__("You can upload max of %s!"), get_max_file_size()); ?>
-                </span>
-                <span class="label label-success">
-                    <?php printf(__("You can storage %s minutes of videos!"), (empty($global['videoStorageLimitMinutes']) ? "unlimited" : $global['videoStorageLimitMinutes'])); ?>
-                </span>
                 <span class="label label-success">
                     <?php printf(__("You have %s minutes of videos!"), number_format(getSecondsTotalVideosLength() / 6, 2)); ?>
                 </span>
